6658537e28c2430a0179d0281a23d6a8693bc603
KentaroKoizumi <kentaroskoizumi@gmail.com>
2022-10-15 16:03:24 +0900

    コードに説明コメントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,38 @@ import r from "../lib/googleApi.json"
 type RES = typeof r
 
 function App() {
+  // useState の定義一覧。
+  // 撮られた画像
   const [image, setImage] = useState<string | null | undefined>(null)
+  // OCR で取得した文字列
   const [ocr, setOcr] = useState<string>('')
+  // ISBN
   const [isbn, setIsbn] = useState<string>('')
+  // Google Books API で取得した書籍のタイトル
   const [title, setTitle] = useState<string>('')
+  // Google Books API で取得した書籍の著者
   const [authors, setAuthors] = useState<string[]>([''])
+  // 学籍番号
   const [studentNo, setStudentNo] = useState<string>('')
+  // カメラが起動しているかどうか
   const [isCamOn, setIsCamOn] = useState<boolean>(false)
   
+  // カメラで用いる ref
   const webcamRef = useRef<Webcam>(null);
 
+  // カメラの設定。画質と，起動するカメラの向き（内カメラか外カメラか）を指定。
   const videoConstraints = {
     width: 1920,
     height: 1080,
     facingMode: "environment"
   };
 
-
+  // 写真が撮られた時に呼び出される処理
   const capture = useCallback(
     () => {
       const imageSrc = webcamRef.current?.getScreenshot();
       if( imageSrc != null){
+        // 撮られた画像が null でなければ，image を更新し，OCR を実行する。
         setImage(imageSrc)
         doOCR(imageSrc)
       }
@@ -39,22 +50,30 @@ function App() {
     [webcamRef]
   )
 
+  // 画像を削除する関数。リトライボタンが押されたときに呼び出される。
   const delImage = () =>{
     setImage(null)
   }
 
+  // OCR してくれる労働者を定義
   const worker = createWorker({
     logger: m => console.log(m),
   })
+
+  // OCR を実行する関数
   const doOCR = async (img: string) => {
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
     const { data: { text } } = await worker.recognize(img);
+
+    // 取得した文字列を ocr に格納
     setOcr(text);
 
+    // 取得した文字列から ISBN の部分を抽出
     const result = text.match(/(I|1)(S|5|s)(B|8|5)N[0-9 -]{9,18}/)
     if(result){
+      // ISBN が取得できた場合，結果から数字のみを抽出して isbn に格納
       let resultString = result[0].toString()
       console.log(resultString)
       resultString = resultString.slice(4).replaceAll("-", "").replaceAll(" ", "")
@@ -62,31 +81,33 @@ function App() {
     }
   }
 
+  // カメラの起動・停止を切り替える関数
   const toggleCam = () => {
     setIsCamOn(!isCamOn)
   }
 
-useEffect(() =>{
-    // console.log(isbn)
-    // console.log(isbn.length)
-    // console.log((isbn.length===9) || (isbn.length===10) || (isbn.length===13))
+  // isbn が更新されたときに呼び出される関数
+  // Google Books API にリクエストを送り，書籍のタイトルと著者を取得する。
+  useEffect(() =>{
+    // isbn の長さで有効な ISBN か検証
     if ((isbn.length===9) || (isbn.length===10) || (isbn.length===13)){ 
-    const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
-    axios.get(url)
-      .then((res: AxiosResponse<RES>) => {
-      // .then((res: any) => {
-        console.log(res)
-        if(res.data.totalItems > 0){
-          console.log(res.data.items[0].volumeInfo.title)
-          setTitle(res.data.items[0].volumeInfo.title)
-          setAuthors(res.data.items[0].volumeInfo.authors)
-        } else {
-          setTitle("")
-          setAuthors([""])
-        }
-      })
-    } else {
-      return
+      const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
+      // Google Books API にリクエストを送る
+      axios.get(url)
+        .then((res: AxiosResponse<RES>) => {
+          console.log(res)
+          // response から，書籍情報が取得できたかで処理を分岐
+          if(res.data.totalItems > 0){
+            console.log(res.data.items[0].volumeInfo.title)
+            // 書籍情報が取得できた場合，タイトルと著者をそれぞれ格納
+            setTitle(res.data.items[0].volumeInfo.title)
+            setAuthors(res.data.items[0].volumeInfo.authors)
+          } else {
+            // 書籍情報が取得できなかった場合，タイトルと著者を空にする
+            setTitle("")
+            setAuthors([""])
+          }
+        })
     }
   }, [isbn])
 
@@ -142,15 +163,7 @@ useEffect(() =>{
         </>
       ) : (
         <>
-          <Box
-            sx={{
-              // width: imageDisplaySize.width,
-              // height: imageDisplaySize.height,
-              bgcolor: "black"
-            }}
-          >
-            <Button onClick={toggleCam} variant="contained">ISBN をカメラで読み取る</Button>
-          </Box>
+          <Button onClick={toggleCam} variant="contained">ISBN をカメラで読み取る</Button>
         </>
       )
     }
