cba939b6ab07e007615c51d7b6613049dc4140e8
Yuto Suzuki <yuto.suzuki.0403@gmail.com>
2022-10-23 17:48:05 +0900

    色を変更

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -7,12 +7,21 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
+// 色のグラデーションをつけるための関数
+function linearGradient(color1: string, color2: string, angle: number) {
+  return `linear-gradient(${angle}deg, ${color1} 0%, ${color2} 100%)`;
+}
 
 export default function ButtonAppBar() {
     return (
         <>
     <Box sx={{ flexGrow: 1, paddingTop:1, paddingBottom:1}}>
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        sx={{
+          background: linearGradient('#004d40', '#009688', 90),
+        }}
+        >
         <Toolbar>
             <Typography variant="h5" component="h1">ラウンジ6F貸出管理システム</Typography>
         </Toolbar>
